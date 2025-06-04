8621c52ed2648da628f681b45942788754fee7b1
nicholaschen09 <nicholas.chen243@gmail.com>
2025-06-04 10:52:33 -0400

    added nav bar

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -4,6 +4,16 @@ import "./Homepage.css";
 const Homepage = () => {
     return (
         <div className="homepage-bg">
+            <nav className="homepage-navbar">
+                <ul>
+                    <li><a href="#demographics">Demographics</a></li>
+                    <li><a href="#highschool">High School</a></li>
+                    <li><a href="#academics">Academics</a></li>
+                    <li><a href="#coop">Co-op</a></li>
+                    <li><a href="#lifestyle">Lifestyle</a></li>
+                    <li><a href="#about">About</a></li>
+                </ul>
+            </nav>
             <div className="star star-purple"></div>
             <div className="star star-green"></div>
             <div className="homepage-content">
